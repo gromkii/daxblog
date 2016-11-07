794b7a6fabef569d4cc667b0d72f01b7dbf667ed
Dax <limeside@gmail.com>
2016-11-07 11:48:38 -0600

    Add server. :rabbit:

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+    app     = express(),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
+
+app.use(bodyParser.json())
+  .use(bodyParser({urlencoded:false})
+  .use(methodOverride('_method'));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is listening.');
+});
